ae1b0c1f66b472364986af638e2af85044b4d117
Mitchell DeJong <mdejong@steelcase.com>
2023-08-03 16:03:03 -0400

    Adding a game function to play 5 rounds, and updated return value to give more context.

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,6 +1,6 @@
 const choices = ["Rock", "Paper", "Scissors"];
-const playerSelection = "rock";//choices[0];
-const computerSelection = getComputerChoice();
+let computerSelection;
+let playerSelection;
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
@@ -11,17 +11,26 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substr(1);
 
+    //Compare the resutls of the selections to determine a winner
     if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return `You Win! ${playerSelection} beats ${computerSelection}.`
+        return `You Win! Your choice of ${playerSelection} beats the Computer's choice of ${computerSelection}.`
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return `You Win! ${playerSelection} beats ${computerSelection}.`
+        return `You Win! Your choice of ${playerSelection} beats the Computer's choice of ${computerSelection}.`
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return `You Win! ${playerSelection} beats ${computerSelection}.`
+        return `You Win! Your choice of ${playerSelection} beats the Computer's choice of ${computerSelection}.`
     } else if (playerSelection === computerSelection) {
         return `It's a tie! You and the computer both chose ${playerSelection}.`
     } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+        return `You Lose! The Computer's choice of ${computerSelection} beats your choice of ${playerSelection}.`
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+ for (let i = 0; i <=4; i++) {
+    playerSelection = prompt('Enter your choice: "Rock", "Paper", or "Scissors"');
+    computerSelection = getComputerChoice();
+    console.log(playRound (playerSelection, computerSelection));
+ }
+}
+
+game();
